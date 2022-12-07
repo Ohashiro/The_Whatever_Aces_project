485a3069965806b3eb011ae0b760abc12ffaa11d
2019gallieree <emma.galliere@student-cs.fr>
2022-12-07 11:06:21 +0100

    Complete the data set import in main.js

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ data_structures = {
     'health_care_index_legatum.csv':{},
     'health_care_index_with_access.csv':{},
     'MedicalRessourcesOECD_processed.csv':{},
-    'number_of_medical_school_and_population.csv':{},
-    'number_of_school_and_health_index.csv':{},
+    // 'number_of_medical_school_and_population.csv':{},
+    // 'number_of_school_and_health_index.csv':{},
     'OECD_health_exp_processed.csv':{},
     'R_D-expend-processed.csv':{},
     'tuition_fee_and_average_salary.csv':{},
@@ -32,93 +32,136 @@ data_structures = {
 Promise.all([
     d3.csv("../data/Average_Monthly_Net_salary.csv", d => {
         return {
-            'Rank': d.rank_salary,
-            'Country': d.country,
-            'Average Monthly Net Salary (US$)': d.salary
+            'rank_salary': d.Rank,
+            'country': d.Country,
+            'salary': +d['Average Monthly Net Salary (US$)']
         }
     }),
     d3.csv("../data/Cost_of_education - Sheet1.csv", d => {
         return {
-            'Country': d.country,
-            'Number schools': d.nb_of_schools,
-            'Total Fees per Year (USD) MIN': d.min_fees,
-            'Total Fees per Year (USD) MAX': d.max_fees
+            'country': d.Country,
+            'nb_of_schools': +d['Number schools'],
+            'min_fees': +d['Total Fees per Year (USD) MIN'],
+            'max_fees': +d['Total Fees per Year (USD) MAX']
         }
     }),
     d3.csv("../data/generalData-processed.csv", d => {
         return {
-            'Country Code': d.code,
-            'Country Name': d.country,
-            'Year': +d.year,
-            'GDP (US$)': +d.gdp,
-            'Population (number)': +d.pop,
-            'HDI': +d.hdi
+            'code': d['Country Code'],
+            'country': d['Country Name'],
+            'year': +d['Year'],
+            'gdp': +d['GDP (US$)'],
+            'pop': +d['Population (number)'],
+            'hdi': +d['HDI']
         }
     }),
     d3.csv("../data/health_care_index_legatum.csv", d => {
         return {
-            'id': d.id,
-            'Country': d.country,
-            'Area': d.area,
-            'rank_2010': d.rank_2010,
-            'rank_2015': d.rank_2015,
-            'rank_2020': d.rank_2020,
-            'rank_2021': d.rank_2021,
-            'score_2010': d.score_2010,
-            'score_2015': d.score_2015,
-            'score_2020': d.score_2020,
-            'score_2021': d.score_2021,
-            'Year': d.year,
-            'HAQ index (IHME (2017))': d.haq_2017,
-            'Code': d.code,
-            'Life expectancy': d.life_expectancy,
+            'country': d.Country,
+            'area': d.Area,
+            'rank_2010': +d.rank_2010,
+            'rank_2015': +d.rank_2015,
+            'rank_2020': +d.rank_2020,
+            'rank_2021': +d.rank_2021,
+            'score_2010': +d.score_2010,
+            'score_2015': +d.score_2015,
+            'score_2020': +d.score_2020,
+            'score_2021': +d.score_2021,
+            'year': +d.Year,
+            'haq_2017': +d['HAQ index (IHME (2017))'],
+            'code': d.Code,
+            'life_expectancy': +d['Life expectancy']
         }
     }),
     d3.csv("../data/health_care_index_with_access.csv", d => {
         return {
-            'id': d.id,
-            'Rank': d.rank,
-            'Country': d.country,
-            'Health Care Index (Overall)': +d.haq,
-            'Infrastructure': +d.infra,
-            'Professionals': +d.pro,
-            'Cost': +d.cost,
-            'Medicine Availability': +d.medicine,
-            'Government Readiness': +d.readiness,
-            'Year': +d.year,
-            'HAQ index (IHME (2017))': +d.haq_2017,
-            'Code': +d.code,
-            'Life expectancy': +d.life_expectancy,
+            'rank': d.Rank,
+            'country': d.Country,
+            'haq': +d['Health Care Index (Overall)'],
+            'infra': +d['Infrastructure'],
+            'pro': +d['Professionals'],
+            'cost': +d.Cost,
+            'medicine': +d['Medicine Availability'],
+            'readiness': +d['Government Readiness'],
+            'year': +d.Year,
+            'haq_2017': +d['HAQ index (IHME (2017))'],
+            'code': +d.Code,
+            'life_expectancy': +d['Life expectancy'],
         }
     }),
     d3.csv("../data/MedicalRessourcesOECD_processed.csv", d => {
+        //TO DO: precise that the csv is formatted with ";" rather than ","
         return {
+            'code': d['CountryCode'],
+            'country': d['CountryName'],
+            'year': +d['Year'],
+            'nb_hospitals': +d['Number of Hospitals'],
+            'nb_public_hospitals': +d['Number of Publicly owned hospitals'],
+            'nb_non_profit_hospitals': +d['Number of Not-for-profit privately owned hospitals'],
+            'nb_profit_hospitals': +d['Number of For-profit privatey owned hospitals'],
+            'nb_beds': +d['Total hospital beds'],
+            'nb_public_beds': +d['Number of Beds in publicly owned hospitals'],
+            'nb_non_profit_beds': +d['Number of beds in not-for-profit privately owned hospitals'],
+            'nb_profit_beds': +d['Number of beds in for-profit privatey owned hospitals'],
+            'nb_generalists': +d['Number of Generalist medical practitioners'],
+            'nb_specialists': +d['Number of Specialist medical practitioners'],
+            'nb_doctors': +d['Number of Medical doctors not further defined'],
+            'nb_nurses': +d['Number of Practising nurses'],
+            'nurses_salary': +d['Remuneration of hospital nurses'],
+            'specialists_salary': +d['Remuneration of specialists'],
+            'generalists_salary': +d['Remuneration of general practitioners'],
         }
     }),
-    d3.csv("../data/number_of_medical_school_and_population.csv", d => {
-        return {
-        }
-    }),
-    d3.csv("../data/number_of_school_and_health_index.csv", d => {
-        return {
-        }
-    }),
+    // d3.csv("../data/number_of_medical_school_and_population.csv", d => {
+    //     return {
+    //         'country': d.Country,
+    //         'nb_schools': d['Number schools'],
+    //         'pop': d['Population (number)'],
+    //     }
+    // }),
+    // d3.csv("../data/number_of_school_and_health_index.csv", d => {
+    //     return {
+    //         '': d['Country'],
+    //         '': d['Number schools'],
+    //         '': d['Total Fees per Year (USD) MIN'],
+    //         '': d['Total Fees per Year (USD) MAX'],
+    //         '': d[''],
+    //         '': d[''],
+    //         '': d[''],
+    //     }
+    // }),
     d3.csv("../data/OECD_health_exp_processed.csv", d => {
         return {
+            'code': d["'Country code'"],
+            'year': +d[" 'Year'"],
+            'financing_scheme': d[" 'Financing scheme'"],
+            'function': d[" 'Function'"],
+            'provider': d[" 'Provider'"],
+            'share_health_expenses': +d[" 'Share of current expenditure on health (in %)'"],
+            'share_gdp': +d[" 'Share of gross domestic product (in %)'"],
         }
     }),
     d3.csv("../data/R_D-expend-processed.csv", d => {
         return {
+            'country': d["'Country'"],
+            'code': d["'Country Code'"],
+            'year': +d["'Year'"],
+            'r_d_category': d["'Category of R&D'"],
+            'r_d_expenses_GOV': +d["'R&D expenses as PC_GDP (total GOV)'"],
+            'r_d_expenses_HES': +d["'R&D expenses as PC_GDP (total HES)''"],
+            'r_d_expenses_BES': +d["'R&D expenses as PC_GDP (total BES)''"],
+            'r_d_expenses_total': +d["'R&D expenses as PC_GDP (total)''"]
         }
     }),
-    d3.csv("../data/tuition_fee_and_average_salary.csv", d => {
-        return {
-        }
-    }),
+    d3.csv("../data/tuition_fee_and_average_salary.csv"),
 ]).then(function(files) {
     for (let i = 0; i < files.length; i++){
-        console.log("data from "+data_sets[i])
-        console.log(files[i]);
+        console.log("data from "+data_sets[i]);
+        file = files[i];
+        for (let j = 0; j < 5; j++){
+            console.log(file[j]);
+        }
+        console.log(file.length);
     }
 }).catch(function(err) {
     console.log(err)
