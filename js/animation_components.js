660b0f9ca761da1ca93f2a730bd5bc79da0d8b4b
“Ohashiro” <egalliere@gmail.com>
2023-01-05 21:48:10 +0100

    Set gdp profiles properly

--- js/animation_components.js
+++ js/animation_components.js
@@ -1,32 +1,17 @@
 export const animationFilter = (data) => {
     let year = d3.select("#yearSlider").node().value;
-    let gdp_profile = d3.select("#gdp").node().value;
     let country = d3.select("#selectCountry").node().value;
     
     // Filter and sorting the new data
     let newData = data.filter(data => data.year == year);
 
     if (country != "All") {
-        newData = newData.filter(data => data.country == country);
+      newData = newData.filter(data => data.country == country);
     } else {
-      let gdp_limit = 1200;
-      if (gdp_profile == 'low') {
-          newData = newData.filter(data => data.gdp/data.population <= 2*gdp_limit);
-      } 
-      if (gdp_profile == 'mid') {
-          newData = newData.filter(function(item) {
-              if (7*gdp_limit >= item.gdp/item.population && item.gdp/item.population  > 2*gdp_limit) {
-                  return true;
-              }
-              return false;
-            })
-      } 
-      if (gdp_profile == 'high') {
-          newData = newData.filter(data => data.gdp/data.population  > 7*gdp_limit);
-      } 
+      newData = filter_data_on_gdp_profile(newData);
     }
 
-    sort = d3.select("#sort").node().value;
+    let sort = d3.select("#sort").node().value;
     
     if (sort == 'alphabet') {
       newData = newData.sort((a, b) => d3.ascending(a.country, b.country));
@@ -38,4 +23,38 @@ export const animationFilter = (data) => {
       newData = newData.sort((a, b) => d3.descending(a.haq, b.haq));
     }
     return newData;
+}
+
+function compute_gdp_profiles(data) {
+  let gdp_per_capita_array = [];
+  for (let i = 0; i < data.length; i++) {
+    gdp_per_capita_array.push(data[i].gdp/data[i].population);
+  }
+  const lowGDPLimit= d3.quantile(gdp_per_capita_array,0.33);
+  const midGDPLimit= d3.quantile(gdp_per_capita_array,0.66);
+  return [lowGDPLimit,midGDPLimit]
+}
+
+function filter_data_on_gdp_profile(data) {
+  let gdp_profile = d3.select("#gdp").node().value;
+  let lowGDPLimit = compute_gdp_profiles(data)[0];
+  let midGDPLimit = compute_gdp_profiles(data)[1];
+  if (gdp_profile == 'low') {
+    let newData = data.filter(data => data.gdp/data.population <= lowGDPLimit);
+    return newData
+  } 
+  if (gdp_profile == 'mid') {
+    let newData = data.filter(function(item) {
+        if (midGDPLimit >= item.gdp/item.population && item.gdp/item.population  > lowGDPLimit) {
+            return true;
+        }
+        return false;
+      })
+    return newData
+  } 
+  if (gdp_profile == 'high') {
+    let newData = data.filter(data => data.gdp/data.population  > midGDPLimit);
+    return newData
+  } 
+  return data
 }
